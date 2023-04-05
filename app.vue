ba3a15ff100a14f5e1fdff5018e90f323dec8cbb
git-morsalin <morsalin.devxhub@gmail.com>
2023-04-05 15:32:41 +0600

    Write basic code in nuxt3

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+  <CustomInput v-model="message" /> {{ message }}
+  <div>
+    <!-- <NuxtWelcome /> -->
+    <h1>Welcome to Nuxt from Me</h1>
+    <div class="bg-red-200">box</div>
+    <Hello helloData=""></Hello>
+  </div>
+</template>
+<script setup>
+  import { ref } from 'vue';
+  let message = ref('hello whats up')
+</script>
---
+++ components/Hello.vue
@@ -0,0 +1,33 @@
+<template>
+    <h1>{{ msg }} ({{ msg.length }})</h1>
+    <p>
+        <input type="text" v-model="msg">
+    </p>
+        <button @click="increment">Count is: {{ count }}</button>
+    <div>
+        
+        <h1>{{ helloData }}</h1>
+        <h2>{{ treeFruit }} ({{ treeFruit.length }})</h2>
+    </div>
+</template>
+<script setup>
+import { ref } from 'vue';
+    const count = ref(0)
+    let msg = ref("Hello Nuxt with laracast")
+    function increment(){
+        count.value++
+    }
+    defineProps({
+        helloData: {
+            type:String,
+            default:"Hello world here free"
+        },
+        treeFruit: {
+            type:String,
+            default:"Jackfruit"
+        }
+    })
+    setTimeout(() => {
+        msg.value = "Laracast for better"
+    }, 3000 );
+</script>
